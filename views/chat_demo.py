fec8ee9ec987a13dc67b1221eb8e3361095b3137
Flash Digital Tech <159139704+flash-digital-tech@users.noreply.github.com>
2024-11-27 12:21:54 -0300

    Update chat_demo.py
    Atualização da API Replicate

--- views/chat_demo.py
+++ views/chat_demo.py
@@ -27,8 +27,8 @@ app = FastAPI()
 
 
 # --- Verifica se o token da API está nos segredos ---
-if 'REPLICATE_API_TOKEN' in st.secrets:
-    replicate_api = st.secrets['REPLICATE_API_TOKEN']
+if 'REPLICATE_API_TOKEN':
+    replicate_api = config('REPLICATE_API_TOKEN')
 else:
     # Se a chave não está nos segredos, define um valor padrão ou continua sem o token
     replicate_api = None
